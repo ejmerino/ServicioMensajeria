6c2cc87a2cb6f0faec7ec0a1573d244cde464afe
Josué Merino Calderón <yoshichicharra11@gmail.com>
2025-01-06 15:29:42 -0500

    Add Springboot dependency + WebSocketConfig

---
+++ Backend/messaging-app/src/test/java/com/espe/messagingapp/MessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espe.messagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
